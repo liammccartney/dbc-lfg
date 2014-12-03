0251d727f7f3739961c22385c79d51eed4e6b293
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 12:40:22 -0500

    Grouping class creates groups

--- app/helpers/grouping.rb
+++ app/helpers/grouping.rb
@@ -1,15 +1,29 @@
 class Grouping
-  attr_reader :group_size, :groups_amount, :students
-  attr_accessor :extra_students
+  attr_reader :group_size, :groups_amount
+  attr_accessor :extra_students, :groups, :students
 
-  def initalize(args)
+  def initialize(args)
     @students = args[:students]
-    @groups_amount = args[:groups_amount]
-    @group_size = args[:target_group_size]
+    @groups_amount = args.fetch(:groups_amount, 4)
+    @group_size = args.fetch(:group_size, 4)
+    @groups = {}
+    @extra_students = []
   end
 
   def get_groups
+    remove_extra_students
 
+    group_number = 1
+
+    shuffle_students.each_slice(4) do |new_group|
+       groups["group_#{group_number}"] = new_group
+        group_number += 1
+    end
+    if extra_students?
+      distribute_extra_students
+    end
+
+    groups
   end
 
   def shuffle_students
@@ -20,5 +34,18 @@ class Grouping
     extra_students = students.pop(students.length % group_size)
   end
 
+  def distribute_extra_students
+    group_number = 1
+
+    extra_students.each do |student|
+      groups["group_#{group_number}"] << student
+      group_number += 1
+    end
+
+  end
+
+  def extra_students?
+    !extra_students.empty?
+  end
 
 end
