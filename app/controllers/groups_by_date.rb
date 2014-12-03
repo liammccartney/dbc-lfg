d0ecd91f10b3c720e0cb3b3e66a2bdca2a9de736
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 16:30:15 -0500

    route calls select_groups_by_date method

--- app/controllers/groups_by_date.rb
+++ app/controllers/groups_by_date.rb
@@ -1,5 +1,8 @@
 get '/groups/:cohort_name/:cohort_id/:date' do
-  @groups = current_user.groups.where(cohort_id: params[:cohort_id])
-  @groups_by_date = @groups.where(:created_at.to_date == params[:date]))
+  groups = current_user.groups.where(cohort_id: params[:cohort_id])
+  @groups_by_date = select_groups_by_date(groups, params[:date])
+  p '**************'
+  p 'GROUPS BY DATE'
+  p @groups_by_date
   erb :'groups/groups_by_date', locals: {cohort_name: params[:cohort_name]}
 end
