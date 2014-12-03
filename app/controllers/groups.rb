f58670fa29a5bdd3d1176011eb1939dd3c033e31
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 12:19:56 -0500

    post route for new group, finds students from Socrates

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -8,3 +8,16 @@ get '/groups/new' do
   # form for generating new groups from a cohort
   erb :'groups/new_group_form'
 end
+
+
+post '/groups/new' do
+  cohort_name = "#{params[:cohort][:name]} #{params[:cohort][:year]}"
+  cohort = Cohort.find_by(name: cohort_name)
+
+  @students = DBC::Cohort.find(cohort.socrates_id).students
+
+  redirect ('/')
+  # students.each do |student|
+  #   Student.find_or_create_by(name: student.name, socrates_id: student.id, cohort_id: cohort.id)
+  # end
+end
