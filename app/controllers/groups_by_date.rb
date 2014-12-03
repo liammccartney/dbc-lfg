b5df7b0b5053762d4c14a67c3fde1355663cf02d
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 16:35:53 -0500

    additional formatting

--- app/controllers/groups_by_date.rb
+++ app/controllers/groups_by_date.rb
@@ -1,8 +1,6 @@
 get '/groups/:cohort_name/:cohort_id/:date' do
   groups = current_user.groups.where(cohort_id: params[:cohort_id])
   @groups_by_date = select_groups_by_date(groups, params[:date])
-  p '**************'
-  p 'GROUPS BY DATE'
-  p @groups_by_date
+
   erb :'groups/groups_by_date', locals: {cohort_name: params[:cohort_name]}
 end
