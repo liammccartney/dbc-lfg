43e5efda6a5eb86a4802858f5e34054f0355a260
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 10:24:18 -0500

    fix typos

--- app/helpers/error_handling.rb
+++ app/helpers/error_handling.rb
@@ -1,9 +1,9 @@
 def display_error
-  error = session[:error]
-  session[:error] = nil
+  error = session[:errors]
+  session[:errors] = nil
 
   if error
-    return erb :'errors/error_display', layout: false, locals: {errors: error}
+    return erb :'errors/_error_display', layout: false, locals: {errors: error}
   else
     return ""
   end
