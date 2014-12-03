c52d4aff6de0dce52b2f999cf8e52f2a04761999
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 10:13:22 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+
+  validates :name, presence: true
+
+  valdates :email, uniqeness: true
+  valdates :email, presence: true
+  validates_format_of :email, {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create}
 end
