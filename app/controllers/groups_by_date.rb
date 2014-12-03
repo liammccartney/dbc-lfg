75f1166abbb8746f78115651164e811e0c3c7b0d
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 17:08:06 -0500

    jquery and ajax to display groups

--- app/controllers/groups_by_date.rb
+++ app/controllers/groups_by_date.rb
@@ -2,5 +2,5 @@ get '/groups/:cohort_name/:cohort_id/:date' do
   groups = current_user.groups.where(cohort_id: params[:cohort_id])
   @groups_by_date = select_groups_by_date(groups, params[:date])
 
-  erb :'groups/groups_by_date', locals: {cohort_name: params[:cohort_name]}
+  erb :'groups/groups_by_date', locals: {cohort_name: params[:cohort_name]}, layout: false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
+  $(".group-by-cohort").click(function(event){
+    event.preventDefault();
 
+    $target = $(event.target);
+
+    $.ajax({
+      url: $target.attr('href'),
+      method: 'GET',
+      data: $target.serialize()
+    }).done(function(response){
+      $target.parent().append(response)
+    });
+  });
+
+  $("ul").on("click", ".group-by-date", function(){
+    event.preventDefault();
+
+    $target = $(event.target);
+
+    $.ajax({
+      url: $target.attr('href'),
+      method: 'GET',
+      data: $target.serialize()
+    }).done(function(response){
+      $target.parent().append(response)
+    });
+  })
 });
