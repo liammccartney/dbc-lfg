f6398071f9c0d6648631dba5dbe85a0e05ddf47b
Liam McCartney <liam.mccartney@gmail.com>
2014-12-03 19:12:58 -0500

    route to update student names

--- app/controllers/edit_routes.rb
+++ app/controllers/edit_routes.rb
@@ -3,6 +3,13 @@ get '/groups/:group_id/edit' do |group_id|
   erb :'groups/edit', locals: {group: group}
 end
 
+put '/groups/:group_id/edit' do |group_id|
+  params[:students].each do |student_id, student_name|
+    Group.find(group_id).students.find(student_id).update(name: student_name)
+  end
+  redirect ("/groups/#{group_id}")
+end
+
 delete '/groups/delete' do
   Group.find(params[:group_id]).destroy
   redirect('/')
